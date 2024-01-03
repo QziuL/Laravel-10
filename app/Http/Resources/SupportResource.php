f4bd2c1f81a7d4d70a5524951a407264cba1da80
QziuL <lf.quinholi2@gmail.com>
2024-01-03 18:18:13 -0300

    add: api rest

---
+++ app/Http/Resources/SupportResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SupportResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'subject' => $this->subject,
+            'content' => $this->body,
+            'status' => $this->status,
+            // 'dt_created' => $this->created_at,
+            'dt_created' => Carbon::make($this->created_at)->format('d-m-Y H:i:s'),
+        ];
+    }
+}
