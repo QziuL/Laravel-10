f7642dc0831436a6abee526ccb48a63bf5eba6f7
QziuL <lf.quinholi2@gmail.com>
2024-01-04 15:36:39 -0300

    api: list pagination

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -7,6 +7,7 @@ use App\DTO\Supports\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Http\Resources\SupportResource;
+use App\Models\Support;
 use App\Services\SupportService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -17,12 +18,25 @@ class SupportController extends Controller
         protected SupportService $service,
     ){}
 
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $supports = $this->service->paginate(
+            page: $request->get('page', 1),
+            totalPerPage: $request->get('per_page', 2),
+            filter: $request->filter,
+        );
+
+        return SupportResource::collection($supports->items())
+                                ->additional([
+                                    'meta' => [
+                                        'total' => $supports->total(),
+                                        'on_first_page' => $supports->onFirstPage(),
+                                        'on_last_page' => $supports->onLastPage(),
+                                        'current_page' => $supports->currentPage(),
+                                        'next_page' => $supports->getNumberNextPage(),
+                                        'previous_page' => $supports->getNumberPreviousPage(),
+                                    ]
+                                ]);
     }
 
     public function store(StoreUpdateSupport $request)
