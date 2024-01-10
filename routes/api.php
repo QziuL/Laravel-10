8bc6691a0ae30a59ab58ea073964868479c3c4df
QziuL <lf.quinholi2@gmail.com>
2024-01-09 21:58:00 -0300

    api: authentication with sactum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/supports', SupportController::class);
+Route::post('/auth', [AuthController::class, 'auth']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/supports', SupportController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'getUser']);
+});
