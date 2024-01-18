b92eb84cdcd7259496bc5e6c4c0f21c0dbe2882e
QziuL <lf.quinholi2@gmail.com>
2024-01-18 17:43:34 -0300

    use: uuid and observer

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use App\Enums\SupportStatus;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Support extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $fillable = [
         'subject',
