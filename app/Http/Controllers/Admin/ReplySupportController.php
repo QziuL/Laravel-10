d9b084843435ac7e36f9dcdc9e8dcd09dbec1a81
QziuL <lf.quinholi2@gmail.com>
2024-01-19 16:49:45 -0300

    get replies from support

--- app/Http/Controllers/Admin/ReplySupportController.php
+++ app/Http/Controllers/Admin/ReplySupportController.php
@@ -3,21 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\ReplySupportService;
 use App\Services\SupportService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ReplySupportController extends Controller
 {
-    public function __construct ( protected SupportService $service ) {}
+    public function __construct ( 
+        protected SupportService $supportService,
+        protected ReplySupportService $replyService 
+    ) {}
 
     public function index(string $id): View
     {
-        if(!$support = $this->service->findOne($id))
+        if(!$support = $this->supportService->findOne($id))
         {
             return redirect()->route('supports.index');
         }
+
+        $replies = $this->replyService->getAllBySupportID($id);
         
-        return view('admin.supports.replies.replies', compact('support'));
+        return view('admin.supports.replies.replies', compact('support', 'replies'));
     }
 }
--- app/Services/ReplySupportService.php
+++ app/Services/ReplySupportService.php
@@ -2,17 +2,22 @@
 
 namespace App\Services;
 use App\DTO\Replies\CreateReplyDTO;
+use App\Repositories\Contracts\ReplyRepositoryInterface;
 use stdClass;
 
 class ReplySupportService 
 {
+    public function __construct(
+        protected ReplyRepositoryInterface $repository
+    ) {}
+
     public function getAllBySupportID(string $supportID): array
     {
-        return [];
+        return $this->repository->getAllBySupportID($supportID);
     }
 
     public function createNew(CreateReplyDTO $dto): stdClass
     {
-        return throw new \Exception('nao implementado');
+        return $this->repository->createNew($dto);
     }   
 }
