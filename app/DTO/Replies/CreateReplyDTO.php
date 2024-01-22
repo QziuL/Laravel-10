e5efe6e0d99c1e9f94aa3ac84a3ccc3f9b416cc8
QziuL <lf.quinholi2@gmail.com>
2024-01-22 15:53:24 -0300

    functions for create/store reply

--- app/DTO/Replies/CreateReplyDTO.php
+++ app/DTO/Replies/CreateReplyDTO.php
@@ -7,6 +7,13 @@ class CreateReplyDTO
     public function __construct(
         public string $supportID,
         public string $content,
-    )
-    {}
+    ) {}
+
+    public static function makeFromRequest(object $request): self
+    {
+        return new self(
+            $request->support_id,
+            $request->content
+        );
+    }
 }
