91f7406b3486e2bf97708fdbf666da85ca310d6c
QziuL <lf.quinholi2@gmail.com>
2024-01-22 15:51:33 -0300

    route for create/store reply

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
+    Route::post('/supports/{id}/replies', [ReplySupportController::class, 'store'])->name('replies.store');
     
     Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
     Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
     Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-    
     Route::post('/supports/create', [SupportController::class, 'store'])->name('supports.store');
     Route::put('/supports/{id}/edit', [SupportController::class, 'update'])->name('supports.update');
-
     Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
 });
 
