2e2f119f064f50369d916e75602c3672b0e37c88
QziuL <lf.quinholi2@gmail.com>
2024-01-30 15:12:46 -0300

    refactoring routes api and create destroy

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,12 @@ Route::post('/auth', [AuthController::class, 'auth']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/supports', SupportController::class);
-    Route::get('/supports/{id}/replies', [ReplySupportApiController::class, 'getRepliesFromSupport']);
-    Route::post('/supports/{id}/replies', [ReplySupportApiController::class, 'storeReplySupport']);
+
+    Route::get('/replies/{support_id}', [ReplySupportApiController::class, 'getRepliesFromSupport']);
+    Route::post('/replies/{support_id}', [ReplySupportApiController::class, 'storeReplySupport']);
+    Route::delete('/replies/{id}', [ReplySupportApiController::class, 'destroy']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::get('/user', [AuthController::class, 'getUser']);
 });
